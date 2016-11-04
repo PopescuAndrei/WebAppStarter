e12b6abe4ab53758ed19b03e7710de8a01da4666
Andrei Ioan Popescu <andrei.popescu93@gmail.com>
2016-11-04 23:35:24 +0200

    switched to a to do app

--- target/WebApp-1.0-SNAPSHOT/js/app.js
+++
@@ -1,9 +0,0 @@
-var webApp = angular.module( 'webApp', ['ui.router']);
-
-webApp.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
-  $urlRouterProvider.otherwise( '/home' );
-})
-
-.run( function run () {
-})
-
